3a6a444ac17e91ba85ad8bc3ba56753ac3fbf904
Paul Inventado <pinventado@fullerton.edu>
2021-10-14 13:47:30 -0700

    Initial Commit

---
+++ VolunteerTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VolunteerTracker
+//
+//  Created by Paul Inventado on 10/14/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
